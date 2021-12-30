f2af95bac772079a89ea37efadf80232fb6d5af2
Johann <johanncf@metalmat.ufrj.br>
2021-12-29 21:36:32 -0300

    App creation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import Post from './Post';
+import { useState, useEffect } from "react"
+
+const user1 = {
+  name: 'Jane Doe',
+  photo: 'https://images.unsplash.com/photo-1640453998519-956952bbc616?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80'
+}
+
+const date = new Date()
 
 function App() {
+
+  const [currentTime, setCurrentTime] = useState(new Date())
+  const [dateProp, setDateProp] = useState()
+
+  setInterval(() => {
+    setCurrentTime(new Date())
+  }, 60000)
+
+  let years = currentTime.getFullYear() - date.getFullYear()
+  let months = currentTime.getMonth() - date.getMonth()
+  let days = currentTime.getDate() - date.getDate()
+  let minutes = currentTime.getMinutes() - date.getMinutes()
+
+  useEffect(() => {
+    if (years === 0 && months === 0 && days === 0 && minutes === 0) {
+      
+      setDateProp('Seconds ago')
+
+    } else if (years === 0 && months === 0 && days === 0) {
+      
+      setDateProp(`${minutes} min`)
+
+    } else if (years === 0 && months === 0) {
+      
+      setDateProp(`${days} days`)
+
+    } else if (years === 0) {
+      
+      setDateProp(`${days} months`)
+
+    } else {
+      
+      setDateProp(`${years} years`)
+
+    }
+    
+  }, [currentTime])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Post user={user1} photo={'https://images.unsplash.com/photo-1467003909585-2f8a72700288?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'} date={dateProp} > Texto de teste pra meu componente de postagem </ Post>
   );
 }
 
---
+++ src/Post/index.jsx
@@ -0,0 +1,51 @@
+import { IconContext } from "react-icons";
+import { GoKebabVertical } from "react-icons/go";
+import { FaRegHeart, FaHeart } from "react-icons/fa";
+import { useState } from "react";
+import "./style.css"
+
+export default function Post({ user, photo, date, children }) {
+
+    const [like, setLike] = useState(true)
+
+    return (
+        <section className="post_container">
+            <header className="post_header">
+                <div className="user">
+                    <div className='user_photo-border'>
+                        <div className='user_photo' style={{ backgroundImage: `url(${user.photo})` }}></div>
+                    </div>
+                    <div className='user_infos'>
+                        <h4 className='name'>{user.name}</h4>
+                        <span className='date'>{date}</span>
+                    </div>
+                </div>
+
+                <div className="menu_button">
+                    <GoKebabVertical />
+                </div>
+            </header>
+            <article className="post_content">
+                <p className="post_text">{children}</p>
+                <img src={photo} alt='post-image' className="post-image" />
+            </article>
+            <div className="actions">
+                <div className="likes-comments_container">
+                    <button className="like" onClick={() => setLike(current => !current)}>
+                        <IconContext.Provider value={{ color: "red", className: "like" }}>
+                            <div className="icon_container">
+                                {
+                                    like ? <FaRegHeart /> : <FaHeart />
+                                }
+                            </div>
+                        </IconContext.Provider>
+                    </button>
+                    <button className="comment">Comments</button>
+                </div>
+                <div className="share_container">
+                    <button className="share">Share</button>
+                </div>
+            </div>
+        </section>
+    );
+}
